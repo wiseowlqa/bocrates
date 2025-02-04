fa7c2d2a4f1484f23735f09a809629598e471ab9
Pooja Mohan M <poojamohanm@gmail.com>
2025-02-04 12:33:42 -0500

    Adding negative scenarios

--- tests/Validate_boc_conversion_rates_api_observation_series.spec.ts
+++ tests/Validate_boc_conversion_rates_api_observation_series.spec.ts
@@ -1,20 +1,25 @@
 import { test, expect, request } from '@playwright/test';
-import { getApiResponseForObservationBySeriesForRecentWeeks } from '../utilities/BankOfCanadaAPI';
+import { getApiResponseForObservationBySeriesForRecentWeeks , validateApiResponse } from '../utilities/BankOfCanadaAPI';
 
 test('Validate observations by series api', async ({ page }) => {
     const numberOfWeeks = 10;
-    const seriesNames = ['FXCADUSD', 'FXUSDCAD', 'FXAUDUSD', 'FXUSDAUD'];
+    const seriesNames = ['FXCADUSD', 'FXUSDCAD', 'FXAUDCAD', 'FXBRLCAD'];
 
     await test.step('Validate observations api for valid series names', async () => {
 
         for (const seriesName of seriesNames) {
-            /*  const response = await request.get('https://www.bankofcanada.ca/valet/observations/FXCADUSD/json?recent_weeks=10');
-              const data = await response.json();
-              const respObservations =  JSON.stringify(data.observations);
-              console.log('Response is : ' + respObservations);
-              expect(data).toHaveProperty('observations'); */
-            const observations = await getApiResponseForObservationBySeriesForRecentWeeks(page, seriesName, numberOfWeeks);
+            const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, seriesName, numberOfWeeks);
+            const observations = JSON.stringify(data.observations);
+            console.log('Response is : ' + observations);
             expect(observations.length).toBeGreaterThan(0);
         }
+    })
+
+    await test.step('Validate response for invalid series names,' , async () => {
+     
+        const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY' , numberOfWeeks);
+        console.log(data);
+        await validateApiResponse(data,'FXUSY');
+
     })
 })
