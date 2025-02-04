0dce89641acbc7297a1d60f7cfd8e8a13feef028
Pooja Mohan M <poojamohanm@gmail.com>
2025-02-04 13:11:50 -0500

    Validatiosn added for positive and negative responses

--- tests/Validate_boc_conversion_rates_api_observation_series.spec.ts
+++ tests/Validate_boc_conversion_rates_api_observation_series.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect, request } from '@playwright/test';
-import { getApiResponseForObservationBySeriesForRecentWeeks , validateApiResponse } from '../utilities/BankOfCanadaAPI';
+import { getApiResponseForObservationBySeriesForRecentWeeks } from '../utilities/BankOfCanadaAPI';
 
 test('Validate observations by series api', async ({ page }) => {
     const numberOfWeeks = 10;
@@ -12,14 +12,32 @@ test('Validate observations by series api', async ({ page }) => {
             const observations = JSON.stringify(data.observations);
             console.log('Response is : ' + observations);
             expect(observations.length).toBeGreaterThan(0);
+            expect(observations.hasOwnProperty('d'));
+            expect(observations.hasOwnProperty('v'));
+            expect(data.observations[0].d).toContain('2024');
+            let firstDateValueInResponse = data.observations[0].d
+            firstDateValueInResponse = new Date(firstDateValueInResponse);
+            // Get the current date
+            let currentDate = new Date();
+            // Compare the dates and validate if response is for dates prior to current date
+            let dateCompare = currentDate > firstDateValueInResponse;
+            expect (dateCompare).toBe(true);
         }
     })
 
-    await test.step('Validate response for invalid series names,' , async () => {
-     
-        const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY' , numberOfWeeks);
+    await test.step('Validate response for invalid series names,', async () => {
+
+        const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', numberOfWeeks);
         console.log(data);
-        await validateApiResponse(data,'FXUSY');
+        expect(data.message).toContain('Series FXUSY not found');
+
+    })
+
+    await test.step('Validate response for invalid number of weeks,', async () => {
+
+        const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', 0);
+        console.log(data);
+        expect(data.message).toContain('Bad recent observations request parameters, you cannot have a recent value less than one');
 
     })
 })
