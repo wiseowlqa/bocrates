d6ff30a701f7195aac703230859a1d746e3fa83f
Pooja Mohan M <poojamohanm@gmail.com>
2025-02-04 13:57:36 -0500

    Single test split into 2 tests for positive and negative validations. Test step added within for-loop to identify validations for each of the series names being validated. This will look nicer in the report

--- tests/Validate_boc_conversion_rates_api_observation_series.spec.ts
+++ tests/Validate_boc_conversion_rates_api_observation_series.spec.ts
@@ -1,13 +1,15 @@
 import { test, expect, request } from '@playwright/test';
 import { getApiResponseForObservationBySeriesForRecentWeeks } from '../utilities/BankOfCanadaAPI';
 
-test('Validate observations by series api', async ({ page }) => {
-    const numberOfWeeks = 10;
-    const seriesNames = ['FXCADUSD', 'FXEURCAD','FXUSDCAD', 'FXAUDCAD', 'FXBRLCAD'];
+const numberOfWeeks = 10;
+const seriesNames = ['FXCADUSD', 'FXEURCAD', 'FXUSDCAD', 'FXAUDCAD', 'FXBRLCAD'];
+
+test('Validate observations by series api with valid inputs for series names and number of weeks', async ({ page }) => {
 
     await test.step('Validate observations api for valid series names', async () => {
 
         for (const seriesName of seriesNames) {
+            await test.step(`Validate response for series ${seriesName}`, async () => {
             const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, seriesName, numberOfWeeks);
             const observations = JSON.stringify(data.observations);
             console.log('Response is : ' + observations);
@@ -21,41 +23,44 @@ test('Validate observations by series api', async ({ page }) => {
             let currentDate = new Date();
             // Compare the dates and validate if response is for dates prior to current date
             let dateCompare = currentDate > firstDateValueInResponse;
-            expect (dateCompare).toBe(true);
+            expect(dateCompare).toBe(true);
+            })
         }
     })
-
-    await test.step('Validate response for invalid series names,', async () => {
-
-        const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', numberOfWeeks);
-        console.log(data);
-        expect(data.message).toContain('Series FXUSY not found');
-
-    })
-
-    await test.step('Validate response for 0 number of weeks,', async () => {
-
-        const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', 0);
-        console.log(data);
-        expect(data.message).toContain('Bad recent observations request parameters, you cannot have a recent value less than one');
-
-    })
-
-    await test.step('Validate response for negative number of weeks,', async () => {
-
-        const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', -1);
-        console.log(data);
-        expect(data.message).toContain('Bad recent observations request parameters, you cannot have a recent value less than one');
-
-    })
-
-    await test.step('Validate response for string value in number of weeks,', async () => {
-
-        const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', 'abcd');
-        console.log(data);
-        expect(data.message).toContain('Bad recent observations request parameters, must be numeric');
-
-    })
-
-
 })
+
+    test('Validate observations by series api with invalid inputs for series names and number of weeks', async ({ page }) => {
+
+        await test.step('Validate response for invalid series names,', async () => {
+
+            const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', numberOfWeeks);
+            console.log(data);
+            expect(data.message).toContain('Series FXUSY not found');
+
+        })
+
+        await test.step('Validate response for 0 number of weeks,', async () => {
+
+            const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', 0);
+            console.log(data);
+            expect(data.message).toContain('Bad recent observations request parameters, you cannot have a recent value less than one');
+
+        })
+
+        await test.step('Validate response for negative number of weeks,', async () => {
+
+            const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', -1);
+            console.log(data);
+            expect(data.message).toContain('Bad recent observations request parameters, you cannot have a recent value less than one');
+
+        })
+
+        await test.step('Validate response for string value in number of weeks,', async () => {
+
+            const data = await getApiResponseForObservationBySeriesForRecentWeeks(page, 'FXUSY', 'abcd');
+            console.log(data);
+            expect(data.message).toContain('Bad recent observations request parameters, must be numeric');
+
+        })
+
+    })
